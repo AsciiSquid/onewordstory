6f96a03193c2952ca49cd1906cf548cd8c61197a
AsciiSquid <thatstupidsquid@gmail.com>
2019-06-11 19:44:02 -0600

    Moved secure data to an unversioned settings.json

--- index.js
+++ index.js
@@ -1,30 +1,5 @@
 const Reader = require("./reader.js");
+const Bot = require("./discord.js");
 
-function test() {
-    const discord = require("discord.js");
-    var client = new discord.Client();
+const settings = require("./settings.json");
 
-    client.on("ready", () => {
-        var testChannel = client.channels.get("587145668846419968");
-        var testReader = new Reader(testChannel, {
-            channel: "587145668846419968",
-            writeChannel: "587145718045736972",
-            bufferSize: 0,
-            fullstop: ".",
-            limitToWord: false
-        });
-
-        testReader.on("collect", (m, r) => {
-            console.log(m);
-            console.log(r.recieved);
-        });
-
-        testReader.on("end", (c, r) => {
-            console.log(r);
-        });
-    });
-
-    client.login("MzQ3NDE0ODY3NDQ5NDc5MTY5.XNo34w.Xr28YpxqMI99PEWR8wBLQ1Vi1Hg");
-}
-
-test();
